8ea85f0ef979a7a5f633370a5c5d923d128c5b6c
Everton Kluczkovski <kluczkovski@hotmail.com>
2019-05-21 15:49:31 +0000

    Week 2 - Unit 3

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,73 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/base/Log",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/core/UIComponent"
+], function (Controller, Log, JSONModel, formatter, Filter, FilterOperator, UIComponent) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
-		onInit: function () {
 
+		formatter: formatter,
+
+		onInit: function () {
+			Log.info("Controller has been initialized.");
+		},
+
+		onBeforeRendering: function () {
+			Log.info("View will shortly be rendered.");
+		},
+
+		onAfterRendering: function () {
+			Log.info("View has been rendered.");
+		},
+
+		onExit: function () {
+			Log.info("Controller will shortly be destroyed.");
+		},
+
+		onPress: function (sValue) {
+			sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
+				var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+				oMessage.show(oResourceBundle.getText("search") + " " + sValue);
+			}.bind(this));
+			
+			Log.info(this);
+			var sCity = this.byId('city').getValue(),
+				sGenre = this.byId('genre').getSelectedItem().getKey(),
+			 	oCalendar = this.byId("calendar"),
+				oRowBinding = oCalendar.getBinding("rows"),
+				oFilterGenre,
+				oFilterCity;
+			Log.info(sCity);
+			Log.info(sGenre);
+			Log.info(oCalendar);
+			Log.info(oRowBinding);
+			
+				
+			// Create filters for genre and city according to user inputs
+			oFilterGenre = sGenre ? new Filter("genre", FilterOperator.EQ, sGenre) : null;
+			oFilterCity = sCity ? new Filter("info", FilterOperator.Contains, sCity) : null;
+			Log.info(oFilterGenre);
+			Log.info(oFilterCity);
+			
+			// Apply genre filter to calendar rows
+			oRowBinding.filter(oFilterGenre);
+			Log.info(oRowBinding);
+		
+
+			// Apply city filter to row appointments
+			var aRows = oCalendar.getAggregation("rows");
+			Log.info(aRows);
+		
+			aRows.forEach(function (oItem) {
+				var oAppointmentsBinding = oItem.getBinding("appointments");
+				oAppointmentsBinding.filter(oFilterCity);
+			});
 		}
+
 	});
 });
